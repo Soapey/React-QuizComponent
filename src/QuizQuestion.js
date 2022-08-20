5faa664791ce0ae602bf4fc4a956f9f509c82ca7
Soapey <granttravissoper@gmail.com>
2022-08-20 11:50:33 +1000

    -a

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -15,14 +15,13 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.quiz_question.answer_options.map((option) => {
-              let answer_option = option;
-              let answer_option_index = option.index;
-            })}
-
-            <QuizQuestionButton
-              button_text={this.props.quiz_question.answer_options[0]}
-            />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton key={index} button_text={answer_option} />
+                );
+              }
+            )}
           </ul>
         </section>
       </main>
