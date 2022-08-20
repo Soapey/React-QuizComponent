fda3ac848683470d25ebebe1726ee7f5e22b5970
Soapey <granttravissoper@gmail.com>
2022-08-20 11:44:37 +1000

    -a

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,10 +18,15 @@ class Quiz extends Component {
 
     return (
       <div>
-        {isQuizEnd ? <QuizEnd /> : null}
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-        />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
     );
   }
