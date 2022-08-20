284c4270703a2fff52e983e8340e4c240a1e2759
Soapey <granttravissoper@gmail.com>
2022-08-20 12:04:22 +1000

    -a

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,16 +5,26 @@ import QuizQuestionButton from "./QuizQuestionButton";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
   }
 
   handleClick(buttonText) {
     if (buttonText == this.props.quiz_question.answer)
       this.props.showNextQuestionHandler();
+
+    this.setState((state) => {
+      return { incorrectAnswer: !state.incorrectAnswer };
+    });
   }
 
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right.</p>
+        ) : null}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
