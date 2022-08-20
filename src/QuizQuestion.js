9104bd294beb917dcc074518ce18beeb7b23a094
Soapey <granttravissoper@gmail.com>
2022-08-20 11:34:16 +1000

    -a

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,10 +10,12 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p>// instruction text goes here</p>
+          <p>{this.props.instruction_text}</p>
         </section>
         <section className="buttons">
-          <ul>//quiz question button logic goes here</ul>
+          <ul>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+          </ul>
         </section>
       </main>
     );
